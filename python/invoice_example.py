c16f7310cc9bb1fa1f4a238781f13eb512c3a111
Shiva Sitamraju <shiva@blockonomics.co>
2017-03-16 10:47:42 +0530

    Using code from http://stackoverflow.com/questions/36762098/how-to-decrypt-password-from-javascript-cryptojs-aes-encryptpassword-passphras
    Encrypting properly using required padding

--- python/invoice_example.py
+++ python/invoice_example.py
@@ -1,10 +1,8 @@
 #!/usr/bin/python
-salt = '7cb8d23734b9d4eb'.decode('hex')
-
-import hashlib, binascii
+from Crypto import Random
 from Crypto.Cipher import AES
 import base64
-from passlib.utils.pbkdf2 import pbkdf1
+from hashlib import md5
 
 BLOCK_SIZE = 16
 
@@ -12,38 +10,46 @@ def pad(data):
     length = BLOCK_SIZE - (len(data) % BLOCK_SIZE)
     return data + (chr(length)*length).encode()
 
-def hasher(algo, data):
-    hashes = {'md5': hashlib.md5, 'sha256': hashlib.sha256,
-    'sha512': hashlib.sha512}
-    h = hashes[algo]()
-    h.update(data)
+def unpad(data):
+    return data[:-(data[-1] if type(data[-1]) == int else ord(data[-1]))]
 
-    return h.digest()
+def bytes_to_key(data, salt, output=48):
+    # extended from https://gist.github.com/gsakkis/4546068
+    assert len(salt) == 8, len(salt)
+    data += salt
+    key = md5(data).digest()
+    final_key = key
+    while len(final_key) < output:
+        key = md5(key + data).digest()
+        final_key += key
+    return final_key[:output]
 
-# pwd and salt must be bytes objects
-def openssl_kdf(algo, pwd, salt, key_size, iv_size):
-    if algo == 'md5':
-        temp = pbkdf1(pwd, salt, 1, 16, 'md5')
-    else:
-        temp = b''
-
-    fd = temp    
-    while len(fd) < key_size + iv_size:
-        temp = hasher(algo, temp + pwd + salt)
-        fd += temp
-
-    key = fd[0:key_size]
-    iv = fd[key_size:key_size+iv_size]
-
-    print('salt=' + binascii.hexlify(salt).decode('ascii').upper())
-    print('key=' + binascii.hexlify(key).decode('ascii').upper())
-    print('iv=' + binascii.hexlify(iv).decode('ascii').upper())
-
-    return key, iv
-
-def encrypt(message):
-    (key, iv) = openssl_kdf('md5', b'hello', salt, 32, 16)   
+def encrypt(message, passphrase):
+    salt = '0b9c83b441172410'.decode('hex')
+    key_iv = bytes_to_key(passphrase, salt, 32+16)
+    key = key_iv[:32]
+    iv = key_iv[32:]
+    print key.encode('hex')
+    print iv.encode('hex')
+    aes = AES.new(key, AES.MODE_CBC, iv)
+    print aes.encrypt(pad(message)).encode('hex')
     aes = AES.new(key, AES.MODE_CBC, iv)
     return base64.b64encode(b"Salted__" + salt + aes.encrypt(pad(message)))
 
-print(encrypt('test'))
+def decrypt(encrypted, passphrase):
+    encrypted = base64.b64decode(encrypted)
+    assert encrypted[0:8] == b"Salted__"
+    salt = encrypted[8:16]
+    key_iv = bytes_to_key(passphrase, salt, 32+16)
+    key = key_iv[:32]
+    iv = key_iv[32:]
+    aes = AES.new(key, AES.MODE_CBC, iv)
+    return unpad(aes.decrypt(encrypted[16:]))
+
+
+password = "hello".encode()
+print (encrypt(b'test ', password))
+#ct_b64 = "U2FsdGVkX1+ATH716DgsfPGjzmvhr+7+pzYfUzR+25u0D7Z5Lw04IJ+LmvPXJMpz"
+#pt = decrypt(ct_b64, password)
+#print("pt", pt)
+#print("pt", decrypt(encrypt(pt, password), password))
